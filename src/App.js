d6505388066453e00300f0bb7e9e511474d3eb81
J-grigri <jurgis.grigorjevs@gmail.com>
2020-03-18 22:16:38 +0700

    basic features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, componentDidMount } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      weather: null
+    }
+  }
+  //get current user location
+  getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {this.currentWeather(position.coords.latitude, position.coords.longitude) })
+  }
+  //get location by geographic coordinates
+async currentWeather (lat, lon) {
+  const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_APIKEY}&units=metric`) ;
+  const data = await resp.json() ; 
+
+  //save data to the state
+  this.setState({weather:data});
+}
+componentDidMount(){
+  this.getLocation();
+}
+render(){
+  console.log("weather", this.state.weather); 
+
+  if (!this.state.weather) return <h1>Loading....... </h1>
+ 
+  else return (
+    <div>
+      <h1>Weather App</h1>
+      <h2>{this.state.weather.name}</h2>
+      <h3>{this.state.weather.weather[0].description}</h3>
+      <h3>{this.state.weather.main.temp}</h3>
     </div>
-  );
+  )
+}
 }
 
 export default App;
